e08f48c74d5cdc7948e8458ba744d4493040fb82
Tanarit Apichokjirasin <ta168023@rutningroup.com>
2026-03-18 23:17:04 +0700

    fix webhook flowVersion sync and session-safe scan flow2

--- src/routes/lineWebhook.js
+++ src/routes/lineWebhook.js
@@ -4,6 +4,7 @@ import {
   getSession,
   setPendingImage,
   clearSession,
+  clearSessionIfFlowVersionMatches,
 } from "../stores/session.store.js";
 
 import { getSavedBirthdate } from "../stores/userProfile.db.js";
